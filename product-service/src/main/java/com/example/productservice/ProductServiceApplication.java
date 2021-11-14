6350d5fd0c1372969c83445771fbfa5f90edce43
Дауытбекова Меруерт <dauytbekovam@gmail.com>
2021-11-14 22:08:48 +0600

    Adding Security to Microservices

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -2,11 +2,12 @@ package com.example.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +16,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableHystrixDashboard
 @EnableHystrix
-
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
