bfb63224374e15ffa0aae67ddcab3d36639ca921
Дауытбекова Меруерт <dauytbekovam@gmail.com>
2021-11-19 22:10:42 +0600

    consuming secured microservice (product-service) via RestTemplate

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -7,7 +7,12 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -30,6 +35,18 @@ public class ProductServiceApplication {
         HttpComponentsClientHttpRequestFactory requestFactory
                 = new HttpComponentsClientHttpRequestFactory();
         requestFactory.setConnectTimeout(3000);
+
+		CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+		credentialsProvider.setCredentials(AuthScope.ANY,
+				new UsernamePasswordCredentials("rest-client", "p@ssword"));
+
+		HttpClient client = HttpClientBuilder
+				.create()
+				.setDefaultCredentialsProvider(credentialsProvider)
+				.build();
+
+		requestFactory.setHttpClient(client);
 
         RestTemplate restTemplate = new RestTemplate(requestFactory);
 
